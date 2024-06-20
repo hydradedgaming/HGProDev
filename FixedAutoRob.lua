e5646d39687217d684ab11dc32989110bb32658b
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-06-20 19:03:49 -0400

    Update FixedAutoRob.lua

--- FixedAutoRob.lua
+++ FixedAutoRob.lua
@@ -134,10 +134,7 @@ if SettingsFile then
 			Settings[i] = v
 		end
 	end
-    if FH_DEBUG then
-        print("[FarmHub (DEBUG)]: Imported new settings from - workspace/Farmhub/AutoRobSettings.json")
-    end
-end
+
 
 -------------------->> Client Player <<--------------------
 
@@ -241,39 +238,6 @@ if FH_DEBUG then
 end
 
 -------------------->> Client Modules <<--------------------
-
-local Modules                   = {
-	UI                          = require(ReplicatedStorage.Module.UI),
-	NPC                         = require(ReplicatedStorage.NPC.NPC),
-	Maid                        = require(ReplicatedStorage.Std.Maid),
-	Store                       = require(ReplicatedStorage.App.store),
-	Raycast                     = require(ReplicatedStorage.Module.RayCast),
-	Vehicle                     = require(ReplicatedStorage.Vehicle.VehicleUtils),
-	GunItem                     = require(ReplicatedStorage.Game.Item.Gun),
-	GuardNPC                    = require(ReplicatedStorage.GuardNPC.GuardNPCShared),
-	TagUtils                    = require(ReplicatedStorage.Tag.TagUtils),
-	GunShopUI                   = require(ReplicatedStorage.Game.GunShop.GunShopUI),
-	CharUtils                   = require(ReplicatedStorage.Game.CharacterUtil),
-	NpcShared                   = require(ReplicatedStorage.GuardNPC.GuardNPCShared),
-	SafeConsts                  = require(ReplicatedStorage.Safes.SafesConsts),
-	CartSystem                  = require(ReplicatedStorage.Game.Cart.CartSystem),
-	TombSystem                  = require(ReplicatedStorage.Game.Robbery.TombRobbery.TombRobberySystem),
-	ItemSystem                  = require(ReplicatedStorage.Game.ItemSystem.ItemSystem),
-	BossConsts                  = require(ReplicatedStorage.MansionRobbery.BossNPCConsts),
-	PuzzleFlow                  = require(ReplicatedStorage.Game.Robbery.PuzzleFlow),
-	AlexChassis                 = require(ReplicatedStorage.Module.AlexChassis),
-	Notification                = require(ReplicatedStorage.Game.Notification),
-	MansionUtils                = require(ReplicatedStorage.MansionRobbery.MansionRobberyUtils),
-	Confirmation                = require(ReplicatedStorage.Module.Confirmation),
-	TeamChooseUI                = require(ReplicatedStorage.TeamSelect.TeamChooseUI),
-	BulletEmitter               = require(ReplicatedStorage.Game.ItemSystem.BulletEmitter),
-	DartDispenser               = require(ReplicatedStorage.Game.DartDispenser.DartDispenser),
-	CharacterAnim               = require(ReplicatedStorage.Game.CharacterAnim),
-	RobberyConsts               = require(ReplicatedStorage.Robbery.RobberyConsts),
-	ButtonService               = require(ReplicatedStorage.App.BigButtonService),
-	MilitaryTurret              = require(ReplicatedStorage.Game.MilitaryTurret.MilitaryTurret),
-	DefaultActions              = require(ReplicatedStorage.Game.DefaultActions),
-}
 
 if FH_DEBUG then
 	print("[FarmHub (DEBUG)]: Loaded enviroment modules.")
@@ -2237,7 +2201,11 @@ local function RobJewelryStore()
                 Character:PivotTo(CFrame.lookAt((box.Position + Vector3.new(0, 2.25, 2.25)), box.Position))
                 BreakFunc = tick()
                 repeat
+		while true do 
                     AttemptPunch()
+wait()
+					end
+		
                     task.wait(0.25)
                 until box.Transparency == 1 or tick() - BreakFunc > 6 or not Robbery.Jewelry.Open 
             end
@@ -2392,7 +2360,6 @@ local function RobPowerPlant()
     
 		SetStatus("Power Plant", "Teleporting to volcano..")
 		VehicleTP(NewCFrame(2286, 19, -2060))
-		task.wait(0.1)
 	
 		VehicleDirectTP(NewCFrame(2275, 25, -2127))
 		VehicleDirectTP(NewCFrame(2213, 25, -2475))
@@ -2454,10 +2421,10 @@ local function RobCargoTrain()
     
     ExitVehicle()
     BringPlatform()
-    task.wait(0.2)
+    task.wait(0.1)
     repeat task.wait() until Gold.CFrame.Z > -5250
     Platform.CFrame = CFrame.new(0, 0, 0)
-    task.wait(0.5)
+    wait()
 
     BreakFunc = tick()
     repeat
@@ -5822,10 +5789,10 @@ local MoneyLbl = StatusTab:CreateTextlabel("Money Earned: $0")
 local TimeLbl = StatusTab:CreateTextlabel("Time Elapsed: 0h/0m")
 local RatesLbl = StatusTab:CreateTextlabel("Estimated Rates: $0/hr")
 
-CreditsSec:CreateTextlabel("@babyfayy333: Scripting")
+CreditsSec:CreateTextlabel("@fuck you fay: Scripting")
 CreditsSec:CreateTextlabel("@itztempy0: Scripting")
 CreditsSec:CreateTextlabel("@harmonicdust: UI Library")
-
+CreditsSec:CreateTextlabel("@hydradedgaming: fixed bugs (fuck fay)")
 AboutSec:CreateKeybind("Toggle Gui", MainWindow.Toggle, { Bind = Enum.KeyCode.RightShift })
 
 
